2613be7811570698d69ee0df6675f0f78cfcf703
Koleaw <gurkin.9696@mail.ru>
2025-12-14 18:43:41 +0300

    Create WeekPage.tsx

---
+++ ver01/src/pages/WeekPage.tsx
@@ -0,0 +1,10 @@
+export default function WeekPage() {
+  return (
+    <div className="space-y-2">
+      <h1 className="text-xl font-semibold">Week</h1>
+      <p className="text-slate-300">
+        Тут будет недельный план (7 колонок), перенос задач между днями и вход в день.
+      </p>
+    </div>
+  );
+}
