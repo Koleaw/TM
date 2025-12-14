883237016cd455c470b8d018e9fa121d85d36789
Koleaw <gurkin.9696@mail.ru>
2025-12-15 00:31:30 +0300

    Update main.tsx

--- ver01/src/main.tsx
+++ ver01/src/main.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./styles.css";
-
-// PWA service worker (vite-plugin-pwa)
-import { registerSW } from "virtual:pwa-register";
-registerSW({ immediate: true });
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
